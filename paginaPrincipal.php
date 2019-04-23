0def7e7c8b3a8e4526c3ade7af0dc67ea994de0b
artthur.nunes <artthur.nunes@outlook.com>
2019-04-23 15:36:18 -0300

    Modificações

--- paginaPrincipal.php
+++ paginaPrincipal.php
@@ -1,4 +1,98 @@
-<?php
-echo 'Entrei !';
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Sistema de contratos HES</title>
+        <!--<link rel="shortcut icon" href="img/comprimido.png"> ADICIONAR ICONE NA ABA DO NAVEGADOR-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        
+        <!-- Não armazena cache -->
+        <meta http-equiv="cache-control" content="no-store, no-cache, must-revalidate, Post-Check=0, Pre-Check=0"> 
+        <meta HTTP-EQUIV="Pragma" CONTENT="no-cache"> 
+        <meta HTTP-EQUIV="Expires" CONTENT="-1">
+            
+        <!-- Bootstrap -->
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="bootstrap/css/style.css" rel="stylesheet" type="text/css"/>    
+                
+        <script type="text/javascript" language="javascript">
+        
+        </script> 
+    </head>
+    <body>
+        <div class="container-fluid"> <!--container da pagina inteira -->
 
-?>
+            <div class="row" id="backgroud-azul-botoes" > <!--Linha do HEADER -->
+                <div class="col-md-2" id="">
+                    <img src="imagens/logo_png.png" width="170" height="40" alt="">
+                </div>
+                <div class="col-md-6 text-center" id="coluna">
+                    <h4><b>Sistema de controle de contratos HES</b></h4>
+                </div>
+                <div class="col-md-3 text-right pt-1" id="coluna">
+                    <h5>Olá <b>USUARIO LOGADO</b></h5>
+                </div>
+                <div class="col-md-1 text-center" id="coluna">
+                    <div class="btn-group" role="group">
+                        <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        --
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                            <a class="dropdown-item" href="#">Link dropdown</a>
+                            <a class="dropdown-item" href="#">Link dropdown</a>
+                        </div>
+                    </div>
+                </div>
+            </div> <!--Linha do HEADER -->           
+
+            <div class="row" id=""> <!-- linha pagina inteira -->
+                <div class="col-md-2" id="backgroud-azul-botoes"> <!-- coluna menu lateral -->
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Active</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                </div> <!-- coluna menu lateral -->
+
+                <div class="col-md-10" id="">
+                    <div class="row" id="">
+                       
+                    
+
+
+
+
+                    </div>
+                </div>
+
+
+                <footer class="footer mt-auto py-2 text-center" id="footer-login">
+                    <div class="container-fluid" id="">
+                        © 2019 Copyright: Desenvolvido pelo setor de Informática - HES
+                    </div>
+                </footer>
+            </div> <!-- linha pagina inteira -->        
+        </div><!--div do container da página -->    
+        
+
+        <script src="bootstrap/jquery/popper.min.js"></script>
+        <script src="bootstrap/jquery/jquery-3.3.1.slim.min.js"></script>
+        <script src="bootstrap/jquery/jquery.js"></script>
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+        <script src="bootstrap/js/main.js"></script>
+        <script src="classesJS/classeLogin.js"></script>       
+
+    </body>
+</html>
+
+
+
